928e121bf304b0fd9bac312115366aefe828be39
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2022-08-13 23:02:17 +0530

    make heading larger

--- src/Portfolio/Portfolio.tsx
+++ src/Portfolio/Portfolio.tsx
@@ -4,14 +4,13 @@ import Manager from './Manager';
 import Search from './Search';
 import HoldingsList from './HoldingsList';
 import Chart from './Chart';
-import { Screen } from '../components';
 
 const Portfolio: React.FC = () => {
   return (
     <Manager>
       <div className="grid grid-cols-2">
         <div className="flex flex-col px-2 mb-16 mt-8">
-          <h1 className="text-2xl font-extrabold">Protfolio by Sector</h1>
+          <h1 className="text-3xl font-extrabold text-slate-700">Protfolio by Sector</h1>
           <div className="sticky top-0 mb-2 pt-2 bg-white rounded-b-md">
             <Search />
           </div>
