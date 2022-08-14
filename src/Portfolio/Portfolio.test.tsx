f78a8625c53e4a34788030130edba9842353cc24
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2022-08-14 11:07:41 +0530

    update test cases to search within holdings section only

--- src/Portfolio/Portfolio.test.tsx
+++ src/Portfolio/Portfolio.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent, waitFor, within } from '@testing-library/react';
 
 import Portfolio from './Portfolio';
 import { setUpServer } from '../api/mock/utils';
@@ -6,19 +6,23 @@ import { searchInput, suggsBoxToBeVisible } from './Search/Search.test';
 
 setUpServer([cleanup]);
 
+const withinHoldingsSection = () => within(screen.getByTestId('holdings-section'));
+
 describe('<Portfolio App />', () => {
   it('should add ticker suggestion from search to holdings list', async () => {
     render(<Portfolio />);
 
     const search = searchInput();
 
+    expect(withinHoldingsSection().queryAllByTestId('holdings-list-item')).toHaveLength(0);
+
     search.focus();
     fireEvent.change(search, { target: { value: 'google' } });
 
-    const opt = (await screen.findAllByRole('option'))[0];
+    const opt = (await withinHoldingsSection().findAllByRole('option'))[0];
     fireEvent.click(opt);
 
-    expect(await screen.findAllByTestId('holdings-list-item')).toHaveLength(1);
+    expect(await withinHoldingsSection().findAllByTestId('holdings-list-item')).toHaveLength(1);
   });
 
   it('should render holdings as pie chart', () => {
