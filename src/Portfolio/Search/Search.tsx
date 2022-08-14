939240642f15394490fec38b4fe97038069aa5c8
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2022-08-14 11:07:54 +0530

    remove unused import

--- src/Portfolio/Search/Search.tsx
+++ src/Portfolio/Search/Search.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useState } from 'react';
-import cx from 'classnames';
 import Autosuggest, { type SuggestionSelectedEventData } from 'react-autosuggest';
 
 import TickerApi from '../../api';
