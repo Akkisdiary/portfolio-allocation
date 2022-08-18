0fdaae13254ffe3fdfce35f25c12cd7ed8ad4530
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2022-08-18 18:40:26 +0530

    remove depricated components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,5 @@ export { default as Screen } from './Screen';
 export { default as Input } from './Input';
 export { default as Button } from './Button';
 export { default as DeleteBtn } from './DeleteBtn';
-export { default as PieChart } from './PieChart';
-export { default as DoughnutChart } from './DoughnutChart';
 export { default as Skeleton } from './Skeleton';
-export { default as SearchIcon } from './Icons/SearchIcon';
 export * from './Icons';
