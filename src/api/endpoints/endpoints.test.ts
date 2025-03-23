8b9440c5e5d934c20ba7eaf0af5c231411b7b3c3
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2025-03-23 21:46:47 +0530

    fix tests

--- src/api/endpoints/endpoints.test.ts
+++ src/api/endpoints/endpoints.test.ts
@@ -7,18 +7,18 @@ afterEach(cleanup);
 describe('Url()', () => {
   it('should add query params to url', () => {
     const url = Url('/path', { key1: 'value1' });
-    expect(url).toBe('http://test.com/path?key1=value1');
+    expect(url).toBe('http://test.xyz/path?key1=value1');
   });
 });
 
 describe('Endpoints()', () => {
   it('should return search url', () => {
     const url = Endpoints.TickerSearch('infy');
-    expect(url).toBe('http://test.com/search?query=infy');
+    expect(url).toBe('http://test.xyz/search?query=infy');
   });
 
   it('should return currency rates url', () => {
     const url = Endpoints.CurrencyRates('inr');
-    expect(url).toBe('http://test.com/currency/inr');
+    expect(url).toBe('http://test.xyz/currency/inr');
   });
 });
